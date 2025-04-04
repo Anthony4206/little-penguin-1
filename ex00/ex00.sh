990a3516ec913904ab6a868606b5ba09d7240c09
root <root@alevasse.local>
2025-04-04 14:43:39 +0200

    First

---
+++ ex00/ex00.sh
@@ -0,0 +1,13 @@
+git clone https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
+cd linux/
+git checkout master
+git pull
+make kernelversion
+make defconfig
+make menuconfig
+make -j$(nproc)
+cp arch/x86/boot/bzImage /boot/vmlinuz-latest
+# Add /boot/vmlinuz-latest in the Menuentry
+reboot
+dmesg
+grep 'CONFIG_LOCALVERSION_AUTO=y' /usr/src/linux/.config
