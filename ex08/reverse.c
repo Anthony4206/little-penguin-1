69eb18f16fddd6d37a60ccbf74ff851fa23e083a
alevasse <anthony.levasseur42@gmail.com>
2025-06-05 14:37:40 +0200

    ex09

--- ex08/reverse.c
+++ ex08/reverse.c
@@ -17,30 +17,30 @@ static ssize_t myfd_write(struct file *fp, const char __user *user,
 			  size_t size, loff_t *offs);
 
 static const struct file_operations myfd_fops = {
-        .owner = THIS_MODULE,
-        .read = myfd_read,
-        .write = myfd_write
+	.owner = THIS_MODULE,
+	.read = myfd_read,
+	.write = myfd_write
 };
 
 static struct miscdevice myfd_device = {
-        .minor = MISC_DYNAMIC_MINOR,
-        .name = "reverse",
-        .fops = &myfd_fops
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = "reverse",
+	.fops = &myfd_fops
 };
 
 static char	str[PAGE_SIZE] = {0};
 
 static int __init myfd_init(void)
 {
-        int retval;
+	int retval;
 
-        retval = misc_register(&myfd_device);
-        return retval;
+	retval = misc_register(&myfd_device);
+	return retval;
 }
 
 static void __exit myfd_cleanup(void)
 {
-        misc_deregister(&myfd_device);
+	misc_deregister(&myfd_device);
 }
 
 ssize_t myfd_read(struct file *fp, char __user *user,
@@ -48,7 +48,7 @@ ssize_t myfd_read(struct file *fp, char __user *user,
 {
 	size_t	len;
 	size_t	i;
-	size_t	ret;
+	ssize_t	ret;
 	char	*tmp;
 
 	if (*offs != 0)
@@ -63,7 +63,6 @@ ssize_t myfd_read(struct file *fp, char __user *user,
 		tmp[i] = str[len - 1 - i];
 	tmp[i] = '\0';
 	ret = simple_read_from_buffer(user, size, offs, tmp, i);
-	*offs = len;
 	kfree(tmp);
 	return ret;
 }
@@ -78,7 +77,7 @@ ssize_t myfd_write(struct file *fp, const char __user *user,
 
 	res = simple_write_to_buffer(str, size, offs, user, size);
 	if (res >= 0)
-		str[size + 1] = '\0';
+		str[res] = '\0';
 	return res;
 }
 
---
+++ ex09/mymounts.c
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/slab.h>
+#include <linux/mount.h>
+#include <linux/nsproxy.h>
+#include <linux/dcache.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <linux/uaccess.h>
+#include <linux/path.h>
+#include <linux/list.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("alevasse");
+MODULE_DESCRIPTION("Get My Mounts!");
+
+static int      mymounts_show(struct seq_file *m, void *v)
+{
+	struct mnt_namespace	*ns = current->nsproxy->mnt_ns;
+	struct mount		*mnt;
+
+	pr_info("Mount namespace: %px\n", ns);
+
+	list_for_each_entry(mnt, &ns->list, mnt_list) {
+		struct vfsmount	*vfsmnt = &mnt->mnt;
+		struct dentry	*root = vfsmnt->mnt_root;
+		struct path	mnt_path = {
+			.mnt = vfsmnt,
+			.dentry = root
+		};
+
+		char 	*buf = kmalloc(PAGE_SIZE, GFP_KERNEL);
+		char	*path;
+
+		if (!buf)
+			return -ENOMEM;
+
+		path = d_path(&mnt_path, buf, PAGE_SIZE);
+
+		pr_info("Mount entry:\n");
+		pr_info("  Devname: %s\n", mnt->mnt_devname);
+		pr_info("  Path   : %s\n", path);
+		pr_info("  Root dentry: %px\n", root);
+		pr_info("  Mount addr : %px\n", mnt);
+
+		kfree(buf);
+	}
+	return 0;
+}
+
+static int	mymounts_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, mymounts_show, NULL);
+}
+
+static const struct file_operations	mymounts_fops = {
+	.owner = THIS_MODULE,
+	.open = mymounts_open,
+	.read = seq_read,
+	.llseek = seq_lseek,
+	.release = single_release
+};
+
+static int	__init init_mymounts(void)
+{
+	proc_create("mymounts", 0444, NULL, &mymounts_fops);
+	printk(KERN_INFO "Success: Proc device has been registered!\n");
+	return 0;
+}
+
+static void      __exit clean(void)
+{
+	remove_proc_entry("mymounts", NULL);
+        printk(KERN_INFO "Cleaning up mymounts module.\n");
+}
+
+module_init(init_mymounts);
+module_exit(clean);
