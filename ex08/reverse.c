d01f984a702169bf693efd79ff94e835bfb45ab9
alevasse <anthony.levasseur42@gmail.com>
2025-06-05 09:36:04 +0200

    ex08

--- ex08/reverse.c
+++ ex08/reverse.c
@@ -11,30 +11,64 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Anthony Levasseur <anthony@ne02ptzero.me>");
 MODULE_DESCRIPTION("Reverse string module");
 
-static char str[PAGE_SIZE];
+static ssize_t myfd_read(struct file *fp, char __user *user,
+			 size_t size, loff_t *offs);
+static ssize_t myfd_write(struct file *fp, const char __user *user,
+			  size_t size, loff_t *offs);
+
+static const struct file_operations myfd_fops = {
+        .owner = THIS_MODULE,
+        .read = myfd_read,
+        .write = myfd_write
+};
+
+static struct miscdevice myfd_device = {
+        .minor = MISC_DYNAMIC_MINOR,
+        .name = "reverse",
+        .fops = &myfd_fops
+};
+
+static char	str[PAGE_SIZE] = {0};
+
+static int __init myfd_init(void)
+{
+        int retval;
+
+        retval = misc_register(&myfd_device);
+        return retval;
+}
+
+static void __exit myfd_cleanup(void)
+{
+        misc_deregister(&myfd_device);
+}
 
 ssize_t myfd_read(struct file *fp, char __user *user,
 		  size_t size, loff_t *offs)
 {
-	size_t t;
-	size_t i;
-	char *tmp2;
+	size_t	len;
+	size_t	i;
+	char	*tmp;
 
-	// Malloc like a boss
-	tmp2 = kmalloc(sizeof(char), GFP_KERNEL);
+	if (*offs != 0)
+		return 0;
+
+	len = strlen(str) - 1;
+	tmp = kmalloc(sizeof(char) * len + 1, GFP_KERNEL);
 	if (!tmp)
 		return -ENOMEM;
 
-	for (i = 0; i < len; i++)
-		tmp[i] = str[len - i - 1];
+        i = 0;
+	while (i++ < len)
+		tmp[i] = str[len - i];
 	tmp[i] = '\0';
 
-	if (copy_to_user(user_buf, tmp, len)) {
+	if (simple_read_from_buffer(user, size, offs, tmp, i)) {
 		kfree(tmp);
 		return -EFAULT;
 	}
 
-	*ppos = len;
+	*offs = len;
 	kfree(tmp);
 	return len;
 }
@@ -44,24 +78,16 @@ ssize_t myfd_write(struct file *fp, const char __user *user,
 {
 	ssize_t res;
 
-	res = 0;
-	res = simple_write_to_buffer(str, size, offs, user, size) + 1;
-	// 0x0 = '\0'
-	str[size + 1] = 0x0;
+	if (size >= PAGE_SIZE)
+		return -EINVAL;
+
+	res = simple_write_to_buffer(str, size, offs, user, size);
+	if (res < 0)
+		return res;
+
+	str[size + 1] = '\0';
 	return res;
 }
-
-static const struct file_operations myfd_fops = {
-	.owner = THIS_MODULE,
-	.read = myfd_read,
-	.write = myfd_write
-};
-
-static struct miscdevice myfd_device = {
-	.minor = MISC_DYNAMIC_MINOR,
-	.name = "reverse",
-	.fops = &myfd_fops
-};
 
 static int __init myfd_init(void)
 {
